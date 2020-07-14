b675f7277730ad5fc16ebde7f3212ad79b4517df
Vishroy Seenarain <vishseenarain@gmail.com>
2020-07-15 03:22:10 +0400

    Command added with format function working

--- main.py
+++ main.py
@@ -1,60 +1,56 @@
-# Simple Text Formatter
+# Simple Text Formatter #
+
+
 import sys
 
 txt_list = []
 
-# main function to execute
-def format(txt_to_format, txt_to_ignore):
-	# txt_to_format = "vishroy // seenarain // web // jira"
-	# txt_to_ignore = "//"
-	word_index = 0
 
-	# txt_ignore get count
-	#
-
-	while(len(txt_to_format) > 0):
-		txt_to_ignore_count = txt_to_format.count(txt_to_ignore)
-
-		if(txt_to_ignore_count > 0):
-			txt_to_ignore_pos = txt_to_format.index(txt_to_ignore)
-
-			txt_formatted = txt_to_format[word_index:txt_to_ignore_pos]
-			word_index = txt_to_ignore_pos + 2
-
-			new_txt_to_format = txt_to_format[word_index:len(txt_to_format)]
-			word_index = 0
-			txt_to_format = new_txt_to_format
-		else:
-			txt_formatted = txt_to_format[word_index:len(txt_to_format)]
-			txt_to_format = ""
-
-		txt_list.append(txt_formatted)
-		print(txt_list)
-
-
-# help function to know input
+# function to know input
 def help():
-	print("format 'text-to-format' 'txt-to-ignore'")
+    print("format 'text-to-format' 'txt-to-ignore'")
 
 
-# format function to format our text
-def g(txt_format, txt_ignore):
-	index = 0
+# function to format our text
+def format(txt_format, txt_ignore):
+    index = 0
+    txt_ignore_len = len(txt_ignore)
+    txt_ignore_count = txt_format.count(txt_ignore)
+
+    while txt_ignore_count > 0:
+        txt_ignore_count = txt_format.count(txt_ignore)
+
+        if txt_ignore_count > 0:
+            txt_ignore_pos = txt_format.index(txt_ignore)
+            txt_formatted = txt_format[index:txt_ignore_pos]
+            index = txt_ignore_pos + txt_ignore_len
+
+            new_txt_format = txt_format[index:len(txt_format)]
+            txt_format = new_txt_format
+            index = 0
+        else:
+            txt_formatted = txt_format[index:len(txt_format)]
+            txt_format = ""
+
+        txt_list.append(txt_formatted)
+
+    print(txt_list)
 
 
-
-# Lifecycle of the program
+# Lifecycle of program
 if __name__ == '__main__':
-	command = sys.argv[1]
+    format()
 
-	# statement to follow commands
-	if(command == "help"):
-		help()
-	elif(command == "format"):
-		txt_format = command = sys.argv[2]
-		txt_ignore = command = sys.argv[3]
+    # command = sys.argv[1]
+    #
+    # # statement to follow commands
+    # if(command == "help"):
+    # 	help()
+    # elif(command == "format"):
+    # 	txt_format = sys.argv[2]
+    # 	txt_ignore = sys.argv[3]
+    #
+    # 	format(txt_format, txt_ignore)
 
-		format(txt_format, txt_ignore)
-
-		for x in txt_list:
-			print(x)
+    for x in txt_list:
+        print(x)
